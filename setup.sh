5ebfcd23675db516ab9cacb16494a9c554bfa8ec
Danny McAteer <mcateerd2@gmail.com>
2025-05-19 21:02:58 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,5 +7,8 @@ uv venv
 # Activate the virtual environment
 source .venv/bin/activate
 
+# setup.sh (runs inside Codex container)
+uv pip install --upgrade "hatchling>=1.24" wheel
+
 # Install the package in development mode with test extras
 uv pip install -e ".[test]"
