637775e67a381cf7d90c53d8148a368fcdda79fa
Dan Mac <mcateerd2@gmail.com>
2025-05-24 11:48:46 -0400

    feat: add reporting module

--- src/meta_agent/evaluation/__init__.py
+++ src/meta_agent/evaluation/__init__.py
@@ -2,10 +2,13 @@
 
 from .execution import ExecutionModule, ExecutionResult
 from .result_collection import CollectionResult, ResultCollectionModule
+from .reporting import ReportingModule, SummaryReport
 
 __all__ = [
     "ExecutionModule",
     "ExecutionResult",
     "ResultCollectionModule",
     "CollectionResult",
+    "ReportingModule",
+    "SummaryReport",
 ]
