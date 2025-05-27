7d71636f0f70c2b29213551796f7017989e4e930
Dan Mac <mcateerd2@gmail.com>
2025-05-27 09:56:40 -0400

    fix: update telemetry client tests

--- pytest_asyncio.py
+++ pytest_asyncio.py
@@ -1,10 +1,8 @@
 import asyncio
-import pytest
+
 
 def pytest_configure(config):
-    config.addinivalue_line(
-        "markers", "asyncio: mark test as running in an event loop"
-    )
+    config.addinivalue_line("markers", "asyncio: mark test as running in an event loop")
 
 
 def pytest_pyfunc_call(pyfuncitem):
