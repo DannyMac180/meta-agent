852481b9d1654d5e8877e50ead24738fdb49c0ce
Danny McAteer <mcateerd2@gmail.com>
2025-06-17 16:00:59 -0400

    update README and add CHANGELOG

--- temp_sandbox_test_code/test_script.py
+++
@@ -1,9 +0,0 @@
-
-import numpy as np # Test dependency
-
-print('Hello from sandbox!')
-print(f'Numpy version: {np.__version__}')
-# print('Writing to stderr', file=sys.stderr)
-# time.sleep(5) # Test timeout
-# sys.exit(1) # Test non-zero exit code
-# with open('/etc/passwd', 'r') as f: print(f.read()) # Test read-only filesystem / disallowed access
